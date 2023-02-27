60af15fa85f23b4d8e839f693cb366194ef2bb2e
Igor Souza Patricio <46011795+igorspatricio@users.noreply.github.com>
2023-02-27 11:31:45 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3000',
+        target: 'https://sistemacarrros.onrender.com',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: { '^/api': '/' },
