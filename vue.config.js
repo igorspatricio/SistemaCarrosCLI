ff669fb095e1a0e9cf7bf208e7f43c34dc428931
Igor Souza Patricio <46011795+igorspatricio@users.noreply.github.com>
2023-02-27 11:41:40 -0400

    proxy para produção

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,28 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'https://sistemacarrros.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: { '^/api': '/' },
+      },
+    },
+  },
+  // Adiciona o proxy para a produção
+  productionSourceMap: false,
+  configureWebpack: {
+    devtool: 'source-map',
+    output: {
+      crossOriginLoading: 'anonymous'
+    },
+    devServer: {
+      proxy: {
+        '^/api': {
+          target: 'https://sistemacarrros.onrender.com',
+          changeOrigin: true,
+          logLevel: 'debug',
+          pathRewrite: { '^/api': '/' },
+        },
       },
     },
   },
